3b2fdca73e37c06e504c72028ab8b1cf15cecd9f
smagdum <sourabhnandkumar.magdum.gcc@hilton.com>
2024-04-10 15:34:36 +0530

    deletion of class property and object

--- class and objects/class_and_objects.py
+++ class and objects/class_and_objects.py
@@ -7,3 +7,10 @@ class Employee:
 emp = Employee(10, "Sourabh")
 print("id:", emp.id, "\nName:", emp.name)
 
+del emp.id
+# This will give you an error as id is deleted
+print("id:", emp.id, "\nName:", emp.name)
+
+del emp
+# this will give you an error as emp is deleted
+print(emp)
